66b1b3a6afdbd71ab58e2c9bb3193d513ec8d42d
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-11-25 18:55:54 -0500

    Conexion de la plantilla al php

--- api.php
+++ api.php
@@ -1,6 +1,25 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
+    $_DATA = (object) [
+        "accept" => apache_request_headers()["accept"]
+    ];
+    if($_DATA->accept == "CrearMenu"){
+        $_DATA->plantilla = <<<HTML
+            <ul>
+                <li><a href="">a</a></li>
+                <li><a href="">b</a></li>
+                <li><a href="">c</a></li>
+                <li><a href="">d</a></li>
+            </ul>
+        HTML;
+    }
+    else if($_DATA->accept == "CrearSection"){
+        $_DATA->plantilla = <<<TEXTO
+            Miguel amet consectetur adipisicing elit. Earum maiores, odit, obcaecati illo ab alias aperiam inventore sed reprehenderit non iusto odio perferendis qui possimus quas reiciendis rem delectus ipsam!
+        TEXTO;
+    };
+    
 
-    var_dump($_SERVER["HTTP_HOST"]);
-?>
+    echo json_encode($_DATA->plantilla, JSON_PRETTY_PRINT);
+?> 
--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("https://comfenalcocursos.000webhostapp.com/portafolio_21_11_2022/api.php");
-    let res = await peticion.text();
-    document.querySelector("pre").innerHTML = res;
-})
+addEventListener("DOMContentLoaded", (e)=>{
+    let url = "https://comfenalcocursos.000webhostapp.com/portafolio_21_11_2022/api.php";
+
+    let menu = async()=>{
+        let Myheader = new Headers();
+        Myheader.set("accept", "CrearMenu");
+        let config = {
+            headers: Myheader, 
+            method: "GET"
+        };
+        let peticion = await fetch(url,config);
+        let res = await peticion.json();
+        document.querySelector("#menu").innerHTML = res;
+    }
+    let secion = async()=>{
+        let Myheader = new Headers();
+        Myheader.set("accept", "CrearSection");
+        let config = {
+            headers: Myheader, 
+            method: "GET"
+        };
+        let peticion = await fetch(url,config);
+        let res = await peticion.json();
+        document.querySelector("#datos").innerHTML = res;
+    }
+
+    Promise.all([menu(),secion()]);
+    
+}) 
